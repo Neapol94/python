47c9a2a96f05a1f0bbfd11f64204080b2a4b8396
Dominika <neapol.blamer@gmail.com>
2020-10-11 20:28:42 +0200

    Zmiany i ćwiczenia

--- jeden.py
+++ jeden.py
@@ -1,4 +1,5 @@
 import sqlite3, random
+from sqlite3 import Error
 
 
 con = sqlite3.connect('C:/sqlite/db/football.db')#db connection
@@ -8,8 +9,8 @@ con.row_factory = sqlite3.Row#columns access via indexes and names
 cur = con.cursor()#creating cursor object
 
 class Team:
-  def __init__(self, teamId, name, potential, stadium, symbol, league, points, goals_scored, goals_against, form):
-    self.teamId = teamId
+  def __init__(self, id, name, potential, stadium, symbol, league, points, goals_scored, goals_against, form):
+    self.id = id
     self.name = name
     self.potential = potential
     self.stadium = stadium
@@ -17,14 +18,9 @@ class Team:
     self.league = league
     self.points = points
     self.goals_scored = goals_scored
-    self.goals_scored = goals_against
+    self.goals_against = goals_against
     self.form = form
 
-  def teamCompare(firstTeamId, secondTeamId):
-    while (firstTeamId.name == secondTeamId.name):
-        id = getTeamId()
-        secondTeamId = teamCreate(id)
-    return firstTeamId, secondTeamId
 
 def getTeamId():
     cur.execute("select count(id) from teams")
@@ -43,12 +39,44 @@ def teamCreate(id):
     return teamObject
 
 
-# def teamCompare(firstTeamId, secondTeamId):
-#     while(firstTeamId.name==secondTeamId.name):
-#         id = getTeamId()
-#         secondTeamId = teamCreate(id)
-#     return firstTeamId, secondTeamId
+def teamCompare(firstTeamId, secondTeamId):
+    while(firstTeamId.name==secondTeamId.name):
+        id = getTeamId()
+        secondTeamId = teamCreate(id)
+    return firstTeamId, secondTeamId
 
+
+def dateGen():
+    def czyPrzestepny(rok):
+        rok = str(rok)
+        if rok % 4 == 0 and (rok % 100 != 0 or rok % 400 == 0):
+            return True
+        else:
+            return False
+
+
+    lata = ('2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020')
+    rok = random.choice(lata)
+
+    miesiace = []
+    for i in range(1,13):
+        if(i<10): miesiace.append("0"+ str(i))
+        else: miesiace.append(str(i))
+    miesiac = random.choice(miesiace)
+
+    dzien = ""
+    if(miesiac=="02"):
+        if(czyPrzestepny(rok)):
+            dzien = random.choice(range(1,30)) #luty 29dni
+        else:
+            dzien = random.choice(range(1, 29)) #luty 28dni
+    if(miesiac in ['01', '03', '05', '07', '08', '10', '12']):
+        dzien = random.choice(range(1, 32)) #31dniowe
+    else: dzien = random.choice(range(1, 31)) #30dniowe
+
+    if (dzien < 10): dzien = "0" + str(dzien)
+
+    return rok + "-" + miesiac + "-" + str(dzien)
 
 def betterTeam(teamHome, teamAway):
     teamHome.potential += 1
@@ -133,17 +161,66 @@ def result(teamHome, teamAway, betterTeamScore, worseTeamScore):
         teamHomeScore = worseTeamScore
         teamAwayScore = betterTeamScore
 
-    return print("%s %d - %d %s" % (teamHome.name, teamHomeScore, teamAwayScore, teamAway.name))
-
-teamHome = teamCreate(getTeamId())
-teamAway = teamCreate(getTeamId())
-team = Team.teamCompare(teamHome, teamAway)
+    return teamHome.name, teamHomeScore, teamAwayScore, teamAway.name
 
 
-probability = winnerProbability(betterTeam(teamHome, teamAway)[0], betterTeam(teamHome, teamAway)[1])
+def saveScores(teamHome, teamHomeScore, teamAwayScore, teamAway):
+    try:
+        score = str(teamHomeScore) + "-" + str(teamAwayScore)
+        #save match
+        cur.execute("INSERT into matches values(?, ?, ?, ?, ?)", (None, dateGen(), score, teamHome.id, teamAway.id))
+
+        # save both teams info update
+        winner = 0
+        if(teamHomeScore>teamAwayScore):
+            winner = teamHome.id
+            cur.execute("UPDATE teams set potential = ?, goals_scored = ?, goals_against = ?, points = ?"
+                        "where id = ?",(teamHome.potential + 1, teamHome.goals_scored + teamHomeScore,
+                            teamHome.goals_against - teamAwayScore, teamHome.points + 3, winner))
+            cur.execute("UPDATE teams set potential = ?, goals_scored = ?, goals_against = ?, points = ?"
+                        "where id = ?", (teamAway.potential - 1, teamAway.goals_scored + teamAwayScore,
+                                         teamAway.goals_against - teamHomeScore, teamAway.points + 0, teamAway.id))
+        elif(teamHomeScore<teamAwayScore):
+            winner = teamAway.id
+            cur.execute("UPDATE teams set potential = ?, goals_scored = ?, goals_against = ?, points = ?"
+                        "where id = ?", (teamAway.potential + 1, teamAway.goals_scored + teamAwayScore,
+                                         teamAway.goals_against - teamHomeScore, teamAway.points + 3, winner))
+            cur.execute("UPDATE teams set potential = ?, goals_scored = ?, goals_against = ?, points = ?"
+                        "where id = ?", (teamHome.potential - 1, teamHome.goals_scored + teamHomeScore,
+                            teamHome.goals_against - teamAwayScore, teamHome.points + 0, teamHome.id))
+        else:
+            winner = 0
+            cur.execute("UPDATE teams set potential = ?, goals_scored = ?, goals_against = ?, points = ?"
+                        "where id = ?", (teamAway.potential + 0, teamAway.goals_scored + teamAwayScore,
+                                         teamAway.goals_against - teamHomeScore, teamAway.points + 1, teamAway.id))
+            cur.execute("UPDATE teams set potential = ?, goals_scored = ?, goals_against = ?, points = ?"
+                        "where id = ?", (teamHome.potential + 0, teamHome.goals_scored + teamHomeScore,
+                                         teamHome.goals_against - teamAwayScore, teamHome.points + 1, teamHome.id))
+    except Error as e:
+        print(e)
+
+
+
+
+
+
+teamHomeObject = teamCreate(getTeamId())
+teamAwayObject = teamCreate(getTeamId())
+team = teamCompare(teamHomeObject, teamAwayObject)
+
+
+probability = winnerProbability(betterTeam(teamHomeObject, teamAwayObject)[0], betterTeam(teamHomeObject, teamAwayObject)[1])
 bts = betterTeamScore(probability)
 wts = worseTeamScore(probability)
-result(teamHome, teamAway, bts, wts)
+# print(teamHomeObject, result(teamHomeObject, teamAwayObject, bts, wts)[1],
+#            result(teamHomeObject, teamAwayObject, bts, wts)[2], teamAwayObject)
+print(result(teamHomeObject, teamAwayObject, bts, wts)[0], result(teamHomeObject, teamAwayObject, bts, wts)[1],
+           result(teamHomeObject, teamAwayObject, bts, wts)[2], result(teamHomeObject, teamAwayObject, bts, wts)[3])
+print(teamHomeObject.name, bts, wts, teamAwayObject.name)
+# saveScores(teamHomeObject, result(teamHomeObject, teamAwayObject, bts, wts)[1],
+#            result(teamHomeObject, teamAwayObject, bts, wts)[2], teamAwayObject)
+
+
 
 con.commit()
 con.close()
--- trzy.py
+++ trzy.py
@@ -1,4 +1,4 @@
-
+import sys
 # def main():
 #     print("Witaj świecie!")
 # if __name__ == "__main__":
@@ -16,9 +16,42 @@ import random
 # sampled_list = random.sample(range(1, 14), 5)
 # print(sampled_list)
 
-def teamCompare(firstTeamId, secondTeamId):
-    while(firstTeamId==secondTeamId):
-        secondTeamId = random.choice(range(1, (14)))
-    return print(firstTeamId, secondTeamId)
+# def teamCompare(firstTeamId, secondTeamId):
+#     while(firstTeamId==secondTeamId):
+#         secondTeamId = random.choice(range(1, (14)))
+#     return print(firstTeamId, secondTeamId)
+#
+# teamCompare(3,3)
 
-teamCompare(3,3)
+
+# slownik = (
+#     {"dzieki":"dziekiinfo","dzieki2":"dziekiinfo2","dzieki3":"dziekiinfo3","dzieki4":"dziekiinfo4"},
+#     {"dzieło":"dziełoinfo","dzieło2":"dziełoinfo2","dzieło3":"dziełoinfo3","dzieło4":"dziełoinfo4"},
+#     {"jujek":"jujekinfo","jujek2":"jujekinfo2","jujek3":"jujekinfo3","jujek4":"jujekinfo4"},
+#     {"ozibambi":"ozibambiinfo","ozibambi2":"ozibambiinfo2","ozibambi3":"ozibambiinfo3","ozibambi4":"ozibambiinfo4"}
+# )
+#
+# for key in slownik:
+#     for keyinfo in key:
+#         print(keyinfo, "-", key[keyinfo])
+
+# numberGenerator = (element**2
+#     for element in range(3)
+#     if(element % 2 == 0)
+# )
+# for item in numberGenerator:
+#     print(item)
+
+names = {"Leszek", "Iza", "Paweł", "Wioletta"}
+celsius = {"c1": -20, "c2": -15, "c3": 0, "c4": 12, "c5": 24, "c6": 21}
+namesLength = {
+    name : len(name)
+    for name in names
+}
+fahrenheit = {
+    key : celsius[key]*1.8+32
+    for key in celsius
+    if celsius[key] > 0
+}
+
+print(fahrenheit)
